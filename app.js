71895c93e1266d45a460d8be9e02914fe3d8d997
bas <bas@bastencate.nl>
2021-02-07 14:38:07 +0100

    jh

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-var constraints = {
-    video: {
-        facingMode: {
-            exact: 'environment'
-        }
-    }, audio: false
-};
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
-
+const devices = navigator.mediaDevices.enumerateDevices()
+let frontDeviceId
+let backDeviceId
+if (devices.length > 0) {
+    /* defaults so all this will work on a desktop */
+    frontDeviceId = devices[0].deviceId;
+    backDeviceId = devices[0].deviceId;
+}
+/* look for front and back devices */
+devices.forEach (device => {
+    if( device.kind === 'videoinput' ) {
+        if( device.label && device.label.length > 0 ) {
+            if( device.label.toLowerCase().indexOf( 'back' ) >= 0 )
+                backDeviceId = device.deviceId
+            else if( device.label.toLowerCase().indexOf( 'front' ) >= 0 )
+                frontDeviceId = device.deviceId
+        }
+    }
+});
+/* close the temp stream */
+const tracks = tempStream.getTracks()
+if( tracks )
+    for( let t = 0; t < tracks.length; t++ ) tracks[t].stop()
+/* open the device you want */
+const constraints = {
+    video: true,
+    deviceId: {exact: backDeviceId }
+}
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
