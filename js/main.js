b1e9317e67e5275381d7f55e1e5367e049346247
bas <bas@bastencate.nl>
2021-02-08 16:15:50 +0100

    jh

--- js/main.js
+++ js/main.js
@@ -101,20 +101,17 @@ function clearphoto() {
 
 function takepicture() {
     var context = canvas.getContext('2d');
-    if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
+    var width = video.width;
+    var height = video.height;
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
 
-        // var data = canvas.toDataURL('image/png');
-        // photo.setAttribute('src', data);
-        var classifier = objectdetect.frontalface;
-        detector = new objectdetect.detector(canvas.width, canvas.height, 1.2, classifier);
-        detectFaces(canvas);
-
-    } else {
-        clearphoto();
-    }
+    // var data = canvas.toDataURL('image/png');
+    // photo.setAttribute('src', data);
+    var classifier = objectdetect.frontalface;
+    detector = new objectdetect.detector(canvas.width, canvas.height, 1.2, classifier);
+    detectFaces(canvas);
 }
 
 function detectFaces(canvas) {
