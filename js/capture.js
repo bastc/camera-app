5c02b1469ee371b01f11d01e7273d0b70db89ca7
bas <bas@bastencate.nl>
2021-02-08 10:02:18 +0100

    jh

--- js/capture.js
+++ js/capture.js
@@ -3,8 +3,8 @@
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
 
-    // var width = 320;    // We will scale the photo width to this
-    // var height = 0;     // This will be computed based on the input stream
+    var width = 800;    // We will scale the photo width to this
+    var height = 0;     // This will be computed based on the input stream
 
     // |streaming| indicates whether or not we're currently streaming
     // video from the camera. Obviously, we start at false.
@@ -36,8 +36,7 @@
 
         video.addEventListener('canplay', function(ev){
             if (!streaming) {
-                height = video.videoHeight;
-                width = video.videoWidth;
+                height = video.videoHeight / (video.videoWidth/width);
 
                 // Firefox currently has a bug where the height can't be read from
                 // the video, so we will make assumptions if this happens.
@@ -46,10 +45,10 @@
                     height = width / (4/3);
                 }
 
-                // video.setAttribute('width', width);
-                // video.setAttribute('height', height);
-                // canvas.setAttribute('width', width);
-                // canvas.setAttribute('height', height);
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
                 streaming = true;
             }
         }, false);
@@ -81,8 +80,6 @@
     // other changes before drawing it.
 
     function takepicture() {
-        var width = video.width;
-        var height = video.height;
         var context = canvas.getContext('2d');
         if (width && height) {
             canvas.width = width;
